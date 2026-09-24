52b51006c8fb6b3ee70564bf8b600c5c9c800bd3
muratsarsekenov21-cloud <muratsarsekenov21@gmail.com>
2026-09-24 18:17:53 +0600

    add a product order orderitem models serializers

--- products/models.py
+++ products/models.py
@@ -6,3 +6,29 @@ class Category(models.Model):
     def __str__(self):
         return self.title
     
+class Product(models.Model):
+    name = models.CharField(max_length=150)
+    description = models.TextField(default='', blank=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    image = models.ImageField()
+    stock = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return self.title
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    status = models.CharField(max_length=20, default="pending")
+    total_price = models.DecimalField(max_digits=10, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return f"Order {self.id}"
+
+class OrderItem(models.Model):
+    user = models.ForeignKey(Order, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity =models.PositiveBigIntegerField() 
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    def __str__(self):
+        return f"{self.product.name} {self.quantity}"
--- products/serializers.py
+++ products/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Category
+from .models import Category, Product, Order, OrderItem
 
 
 
@@ -7,4 +7,19 @@ from .models import Category
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
+        fields = "__all__"
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = "__all__"
+
+class OrderSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Order
+        fields = "__all__"
+
+class OrderItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = OrderItem
         fields = "__all__"
